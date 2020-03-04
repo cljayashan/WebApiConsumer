b701fb4644d233200d364be386818b71fb01a0b0
Jayashan Liyanage <laksrijayashan@gmail.com>
2020-03-04 06:57:06 +0530

    2 new components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ListStudentComponent } from './list-student/list-student.component';
+import { AddNewStudentComponent } from './add-new-student/add-new-student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'list', component: ListStudentComponent },
+  { path: 'addnew', component: AddNewStudentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListStudentComponent } from './list-student/list-student.component';
+import { AddNewStudentComponent } from './add-new-student/add-new-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListStudentComponent,
+    AddNewStudentComponent
   ],
   imports: [
     BrowserModule,
